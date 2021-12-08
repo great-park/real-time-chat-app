c346338dc182fe2fa589421266f183c37fbe06ae
great-park <cksgh1735@naver.com>
2021-12-08 16:02:52 +0900

    서버 및 프론트엔드 설정

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname+ "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req,res) => res.render("home")); //home.pug를 render 해주는 route handler
+
+const handelListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handelListen); 
