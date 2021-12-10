2357ff34ac5766c59deea53b56b0120a02bb9cb4
great-park <cksgh1735@naver.com>
2021-12-11 00:32:03 +0900

    frontend, backend에서 socket 저장

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,4 @@
-alert("hi");
+//frontend
+//server로 socket 보내고 받기
+//여기서의 socket - 서버로의 connection
+const socket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import WebSocket from "ws";
 import http from "http";
 
 const app = express();
@@ -10,4 +11,21 @@ app.get("/", (req,res) => res.render("home")); //home.pug를 render 해주는 ro
 app.get("/*", (req,res)=> res.redirect("/")); //redirection
 
 const handelListen = () => console.log(`Listening on http://localhost:3000`);
+
+//express application으로 wetsocket을 만들 서버 만들어서 접근하기
+// 같은 서버에서 http와 ws 작동
+const server = http.createServer(app); //http 서버 
+//webSocket server
+const wss = new WebSocket.Server({server}); //동일한 포트 내 http, ws request 처리 
+
+//특정 event -> 지정한 콜백을 실행
+//ws에서 정해진 event와 콜백명을 쓰면 됨
+//frontend로 socket 주고 받기
+function handelConnection(socket){  //여기서의  socket - 브라우저와의 connetction
+    console.log(socket);
+}
+
+wss.on("connection", handelConnection);
+
+server.listen(3000, handelListen);
 
