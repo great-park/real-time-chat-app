3ee702c79218406906d491989775e1877176f922
great-park <cksgh1735@naver.com>
2021-12-11 00:52:29 +0900

    websocket message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,19 @@
-//frontend
 //server로 socket 보내고 받기
 //여기서의 socket - 서버로의 connection
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("Hello from the browser");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -5,27 +5,31 @@ import http from "http";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname+ "/views");
+app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home")); //home.pug를 render 해주는 route handler
-app.get("/*", (req,res)=> res.redirect("/")); //redirection
+app.get("/", (req, res) => res.render("home")); //home.pug를 render 해주는 route handler
+app.get("/*", (req, res) => res.redirect("/")); //redirection
 
 const handelListen = () => console.log(`Listening on http://localhost:3000`);
 
 //express application으로 wetsocket을 만들 서버 만들어서 접근하기
 // 같은 서버에서 http와 ws 작동
-const server = http.createServer(app); //http 서버 
+const server = http.createServer(app); //http 서버
 //webSocket server
-const wss = new WebSocket.Server({server}); //동일한 포트 내 http, ws request 처리 
+const wss = new WebSocket.Server({ server }); //동일한 포트 내 http, ws request 처리
 
-//특정 event -> 지정한 콜백을 실행
-//ws에서 정해진 event와 콜백명을 쓰면 됨
+//특정 event -> 지정한 콜백을 실행, ws에서 정해진 event와 콜백명을 쓰면 됨
 //frontend로 socket 주고 받기
-function handelConnection(socket){  //여기서의  socket - 브라우저와의 connetction
-    console.log(socket);
-}
+//여기서의  socket - 브라우저와의 connetction
 
-wss.on("connection", handelConnection);
+wss.on("connection", (socket) => {
+  //listenr 설정
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello");
+});
 
 server.listen(3000, handelListen);
-
