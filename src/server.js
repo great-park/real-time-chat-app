08295314c4ef6886410303b5fa26c377e96fb56c
great-park <cksgh1735@naver.com>
2021-12-12 23:50:21 +0900

    edit listener

--- src/server.js
+++ src/server.js
@@ -18,17 +18,21 @@ const server = http.createServer(app); //http 서버
 //webSocket server
 const wss = new WebSocket.Server({ server }); //동일한 포트 내 http, ws request 처리
 
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+function onSocketMessage(message) {
+  console.log(message.toString("utf8"));
+}
 //특정 event -> 지정한 콜백을 실행, ws에서 정해진 event와 콜백명을 쓰면 됨
 //frontend로 socket 주고 받기
 //여기서의  socket - 브라우저와의 connetction
-
 wss.on("connection", (socket) => {
   //listenr 설정
   console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
-  });
+  socket.on("close", onSocketClose);
+  socket.on("message", onSocketMessage);
   socket.send("hello");
 });
 
