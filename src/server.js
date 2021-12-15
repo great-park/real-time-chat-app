6d885f640bc464e77d1bded024860b377ecf05b5
great-park <cksgh1735@naver.com>
2021-12-16 00:00:53 +0900

    서버와 user 사이 private한 room이 있음을 확인

--- src/server.js
+++ src/server.js
@@ -15,14 +15,15 @@ const handelListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 
-//socket.io server API 문서 참고
+//socket.io server API 문서 참고 ->사용해보니 websocket보다 기능이 많고 편리하다.
 io.on("connection", (socket) => {
   socket.onAny((event) => {
-    console.log(`Socket event:${event}`);
+    console.log(`Socket Event:${event}`);
   });
   socket.on("enter_room", (roomName, done) => {
     //event의 name, front에서 전달받은 JSON objet와 콜백
     //backend에서 콜백을 호출하지만 frontend에서 실행됨
+    console.log(socket.id); //확인 결과, user의 id는 user가 있는 room의 id와 동일하다
     console.log(socket.rooms); // Set { <socket.id> }
     socket.join(roomName);
     console.log(socket.rooms); // Set { <socket.id>, "room1" }
