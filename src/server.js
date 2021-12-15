abb51020d8da93bbb335e46cd21b1f9feacd2183
great-park <cksgh1735@naver.com>
2021-12-15 23:56:32 +0900

    socket.room

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import { SocketAddress } from "net";
 import SocketIO from "socket.io";
 
 const app = express();
@@ -14,11 +15,17 @@ const handelListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 
+//socket.io server API 문서 참고
 io.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket event:${event}`);
+  });
   socket.on("enter_room", (roomName, done) => {
     //event의 name, front에서 전달받은 JSON objet와 콜백
     //backend에서 콜백을 호출하지만 frontend에서 실행됨
+    console.log(socket.rooms); // Set { <socket.id> }
     socket.join(roomName);
+    console.log(socket.rooms); // Set { <socket.id>, "room1" }
   });
 });
 
