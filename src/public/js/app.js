5561aa741f79c8483db4c6ee1dbea2076de68ced
great-park <cksgh1735@naver.com>
2021-12-18 23:56:46 +0900

    add event -bye

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -35,3 +35,7 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("Welcome", () => {
   addMessage("Someone joined!");
 });
+
+socket.on("bye", () => {
+  addMessage("Someone left");
+});
